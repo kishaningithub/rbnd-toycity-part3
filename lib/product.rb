61be8af6359e6ae2d78fffed7ec9f3b47ed148c1
Kishan B <b.kishan.n@gmail.com>
2016-01-30 07:47:37 +0000

    feat(find_by_title, in_stock?): Implement find_by_title and in_stock? methods

--- lib/product.rb
+++ lib/product.rb
@@ -1,10 +1,35 @@
 class Product
-    @@products = []
-    def initialize(options = {})
-        @@products << self
-    end
-    
-    def self.all
-        @@products
-    end
+  attr_reader :title, :price, :stock
+
+  @@products = []
+
+  def initialize(options={})
+    @title = options[:title]
+    @price = options[:price]
+    @stock = options[:stock]
+    add_to_products
+  end
+
+  def self.all
+    @@products
+  end
+  
+  def self.find_by_title(title)
+    @@products.select{|product| product.title == title}.first
+  end
+
+  def in_stock?
+    @stock > 0
+  end
+  
+  private 
+
+  def add_to_products
+    raise DuplicateProductError, "'#{@title}' already exists." unless !duplicate?
+    @@products << self
+  end
+  
+  def duplicate? 
+    @@products.map {|product| product.title}. include? @title
+  end
 end
