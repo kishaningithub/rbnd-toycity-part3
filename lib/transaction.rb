ff3793251f54c32050fb853de11384f09337111d
Kishan B <b.kishan.n@gmail.com>
2016-01-30 08:50:27 +0000

    feat(find): Implementation of find in transaction class

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -2,15 +2,19 @@ class Transaction
   attr_reader :id, :product, :customer
   
   @@transactions = []
+
+  def initialize(customer, product)
+    @customer = customer
+    @product = product
+    add_to_transactions
+  end
   
   def self.all
     @@transactions
   end
   
-  def initialize(customer, product)
-    @customer = customer
-    @product = product
-    add_to_transactions
+  def self.find(id)
+    @@transactions[id - 1]
   end
   
   private
